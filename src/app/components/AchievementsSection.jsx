1739050c484a31c29dfa928e0762319847f5bb0a
hamza <hamzapratama000@gmail.com>
2024-10-16 15:13:59 +0700

    edit

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -12,13 +12,13 @@ const AnimatedNumbers = dynamic(
 const achievementsList = [
   {
     metric: "Projects",
-    value: "100",
+    value: "10",
     postfix: "+",
   },
   {
     prefix: "~",
-    metric: "Users",
-    value: "100,000",
+    metric: "Hours of sleep/day:",
+    value: "",
   },
   {
     metric: "Certificates",
